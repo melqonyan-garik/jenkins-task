0074015bf4ad58aaeede1cafdd6deef096d73633
Garik Melqonyan <garik_melqonyan@epam.com>
2023-10-31 11:43:36 +0400

    add tomcat dependency

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -5,12 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @GetMapping("/hello")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+                .mapToObj(i -> "Hello number " + i)
+                .collect(Collectors.toList());
+    }
 
     @GetMapping("/create")
     public String create() {
